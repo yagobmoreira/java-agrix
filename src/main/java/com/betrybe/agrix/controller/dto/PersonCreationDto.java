526f32033c38324e68d8e053698c66f25c037c2f
Yago Moreira <yagomoreira@gmail.com>
2024-05-13 15:30:42 -0300

    feat: add POST /persons for create a person

---
+++ src/main/java/com/betrybe/agrix/controller/dto/PersonCreationDto.java
@@ -0,0 +1,18 @@
+package com.betrybe.agrix.controller.dto;
+
+import com.betrybe.agrix.entity.Person;
+import com.betrybe.agrix.security.Role;
+
+/**
+ * The type Person creation dto.
+ */
+public record PersonCreationDto(
+    String username,
+    String password,
+    Role role
+) {
+
+  public Person toEntity() {
+    return new Person(username, password, role);
+  }
+}
